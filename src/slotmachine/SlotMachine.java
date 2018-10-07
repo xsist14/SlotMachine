b2e7a5a1739b80172e1af734a5bf3ed76cc875ad
Jason Guest <jason@rgrmarketing.com>
2018-10-07 00:15:38 -0700

    first commit

---
+++ src/slotmachine/SlotMachine.java
@@ -0,0 +1,23 @@
+
+package slotmachine;
+
+
+public class SlotMachine {
+    boolean isLeverPulled;
+    String cherry;
+    String bell;
+    String star;
+    String orange;
+    String grape;
+    String lemon;
+    String luckySeven;
+    String [] reelOne;
+    String [] reelTwo;
+    String[] reelThree;
+    
+    
+    public static void main(String[] args) {
+
+    }
+    
+}
